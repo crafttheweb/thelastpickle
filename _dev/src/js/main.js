f6d48112d601bcb0afe7951b3a00acae5833bada
Darren Wood <darren@thefold.co.nz>
2016-05-24 10:08:54 +1200

    Tweak to contact page
    And slight font tweak to the code blocks

--- _dev/src/js/main.js
+++ _dev/src/js/main.js
@@ -3,41 +3,6 @@
 
     var TLP = TLP || {};
 
-    // Small script that pushed the scroll bar ouside of the code block area
-    // making things look a lot tidier when scroll bars are showing.
-    TLP.codeBlocks = {
-
-        // create invisible box to measure scroll bar width
-        setScrollbarWidth: function() {
-        
-            // Create the measurement node
-            var scrollDiv = document.createElement("div");
-            scrollDiv.className = "scrollbar-measure";
-            document.body.appendChild(scrollDiv);
-
-            return scrollDiv.offsetWidth - scrollDiv.clientWidth;
-        },
-
-        // does an element have overflow?
-        isOverflowed: function(element) {
-            return element.scrollWidth > element.clientWidth;
-        },
-
-        // run all the things
-        init: function() {
-            var $codeBlock = $(".highlighter-rouge pre");
-
-            $codeBlock.each(function() {
-                var $this = $(this);
-
-                if (TLP.codeBlocks.isOverflowed($this[0])) {
-                    $this.css("bottom", "-"+TLP.codeBlocks.setScrollbarWidth()+"px");
-                }
-            })
-        }
-
-    };
-
     TLP.images = {
         init: function() {
           // loop through all images in the .content element
@@ -80,9 +45,6 @@
     };
 
     TLP.citations.init();
-
-    // get those code blocks looking nice
-    // TLP.codeBlocks.init();
 
     // using external JQ plugin to check if images are loaded
     // images are stupid btw.
